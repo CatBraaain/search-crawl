c14fc2161e7c7a8bef95b9766390836ca9dd7d91
CatBraaain <84499939+CatBraaain@users.noreply.github.com>
2025-08-25 14:25:33 +0900

    test: test search without cache

--- tests/search/test_search.py
+++ tests/search/test_search.py
@@ -1,11 +1,26 @@
-from search_crawl_client import DefaultApi, GeneralSearchRequest, ImageSearchRequest
+from search_crawl_client import (
+    CacheConfig,
+    DefaultApi,
+    GeneralSearchRequest,
+    ImageSearchRequest,
+)
 
 
 async def test_search_general(api: DefaultApi):
-    res = await api.search_general(GeneralSearchRequest(q="ping"))
+    res = await api.search_general(
+        GeneralSearchRequest(
+            q="ping",
+            cache_config=CacheConfig(readable=False, writable=False),
+        )
+    )
     assert isinstance(res, list) and len(res) > 0
 
 
 async def test_search_images(api: DefaultApi):
-    res = await api.search_images(ImageSearchRequest(q="ping"))
+    res = await api.search_images(
+        ImageSearchRequest(
+            q="ping",
+            cache_config=CacheConfig(readable=False, writable=False),
+        )
+    )
     assert isinstance(res, list) and len(res) > 0
